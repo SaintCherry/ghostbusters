2d1144392af70e984a2dd3120a202cfc1ad7b4f2
SaintCherry <46715442+SaintCherry@users.noreply.github.com>
2020-08-15 02:08:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    const mMenuBtn = $(".mobile_buttons");
+    const mMenu = $(".curtain")
+    mMenuBtn.on('click', function() {
+        mMenu.toggleClass("active")
+        $("body").toggleClass("no-scroll")
+    });
+    var mySwiper = new Swiper(".swiper-container", {
+        speed: 500,
+        slidesPerView: 4,
+        spaceBetween: 25,
+        loop: true,
+        grabCursor: true,
+        autoHeight: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        breakpoints: {
+            992: {
+                slidesPerView: 4,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            612: {
+                slidesPerView: 2,
+            },
+            312: {
+                slidesPerView: 1,
+            },
+
+        }
+    });
+});
